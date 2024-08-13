2748b81e60366b1ab89311b8374656e05cf99b36
sakivatansever <53531114+sakivatansever@users.noreply.github.com>
2024-08-13 19:43:36 +0300

    Delete ConstructionManagement.MVC directory

--- ConstructionManagement.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ConstructionManagement.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
